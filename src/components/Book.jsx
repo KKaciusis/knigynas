6fffaec647f192b14fba665a8eb86cae7a5265a4
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-23 10:03:13 +0300

    book style

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -5,9 +5,10 @@ function Book(props) {
         <div className="todo">
             <div className="author"><h4>{props.data.author}</h4></div>
             <div className="title">{props.data.title}</div>
-            <img src={props.img}></img>
-            <button className="status" onClick={()=> props.buy(props.id)}>Pirkti</button>
-            <div className="price"><h4>Kaina:  </h4>{props.data.price}</div>   
+            <img src={props.img} alt="Kojniu fabrikas uzdarytas"></img>
+            <button className="btn" onClick={()=> props.buy(props.id)}><span class="noselect">Pirkti</span></button>
+            <div className="price"><h4>Kaina:</h4>&nbsp;{props.data.price}</div>
+            
         </div>
     );
 };
